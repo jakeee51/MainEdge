9f1c6a3abbb07149bf97f1184ed29bfb2e041120
jakeee51 <jakemorfe@gmail.com>
2018-09-05 18:08:42 -0400

    App Complete

--- setup.py
+++ setup.py
@@ -17,18 +17,38 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-executables = [cx_Freeze.Executable("MainEdge.py", base = base, icon = "C:\\Users\\DaviMabi\\Desktop\\MainEdge\\nypaLOGO.ico")]
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "MainEdge",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]\\MainEdge.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     "TARGETDIR"               # WkDir
+     )
+    ]
+msi_data = {"Shortcut": shortcut_table}
+bdist_msi_options = {"data": msi_data}
+
+executables = [cx_Freeze.Executable("MainEdge.py", base = base, icon = "C:\\Users\\DaviMabi\\Desktop\\MainEdge App\\nypaLOGO.ico")]
 
 os.environ['TCL_LIBRARY'] = r'C:\\Program Files\\Python36\\tcl\\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\\Program Files\\Python36\\tcl\\tk8.6'
 
 cx_Freeze.setup(
-    name = "MainEdge-Installer",
+    name = "MainEdge",
     options = {"build_exe": {"packages": ["tkinter", "matplotlib", "pandas", "numpy", "time"],
                              "include_files": [os.path.join('C:\\Program Files\\Python36', 'DLLs', 'tk86t.dll'),
                                                os.path.join('C:\\Program Files\\Python36','DLLs', 'tcl86t.dll'),
-                                               "nypa2.png", "nypa4.png"]}},
+                                               "nypa2.png", "nypa4.png"]},
+               "bdist_msi": bdist_msi_options,},
     version = "0.1",
+    author = "David J. Morfe"
     description = "A Human Resources Employee Database Manager",
     executables = executables
     )
